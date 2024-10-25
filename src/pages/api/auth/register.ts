a7af38e34ed5bbb7499197f0948d47225b65615f
James Baskerville <jameswbaskerville@gmail.com>
2024-10-25 16:22:46 +0800

    Add supabase auth

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,7 @@
+export const prerender = false;
+// All registering will be done by server admin
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+    return redirect("/signin");
+};
