232b1b8d8ed48fbe85856a745ac9ba35adcca742
James Baskerville <jameswbaskerville@gmail.com>
2024-10-28 13:47:58 +0800

    fix auth

--- src/pages/api/auth/signout.ts
+++
@@ -1,9 +0,0 @@
-// With `output: 'hybrid'` configured:
-export const prerender = false;
-import type { APIRoute } from "astro";
-// TODO
-export const GET: APIRoute = async ({ cookies, redirect }) => {
-    cookies.delete("sb-access-token", { path: "/" });
-    cookies.delete("sb-refresh-token", { path: "/" });
-    return redirect("/login");
-};
